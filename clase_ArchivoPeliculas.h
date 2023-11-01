16165de2e9779f209923122c71d30fb4851cd76e
Gianluca Ezequiel Gallino <gianlucaegallino@gmail.com>
2023-11-01 15:46:28 -0300

    Finish archivo classes implementation

--- clase_ArchivoPeliculas.h
+++ clase_ArchivoPeliculas.h
@@ -1,6 +1,110 @@
 #ifndef CLASE_ARCHIVOPELICULAS_H_INCLUDED
 #define CLASE_ARCHIVOPELICULAS_H_INCLUDED
 
+class ArchivoPeliculas {
+  private:
+    char _nombre[30];
+  public:
+    ArchivoPeliculas(const char *nom) {
+        strcpy(_nombre,nom);
+    }
+    const char *getNombre() {
+        return _nombre;
+    }
+    void setNombre(const char *nom) {
+        strcpy(_nombre,nom);
+    }
+
+    bool listarRegistros() {
+        Pelicula reg;
+        FILE *p=fopen(_nombre,"rb");
+        if (p==NULL) {
+            cout<<"ERROR DE ARCHIVO"<<endl;
+            system("pause");
+            return false;
+        }
+        fseek(p, 0, 1);
+        while(fread(&reg, sizeof reg, 1, p)==1) {
+            if (reg.getEstado()==true)reg.Mostrar();
+        }
+        fclose(p);
+        return true;
+    }
+
+    bool agregarRegistro(Pelicula reg) {
+        FILE *p=fopen(_nombre,"ab");
+        if (p==NULL) {
+            cout<<"ERROR DE ARCHIVO"<<endl;
+            system("pause");
+            return false;
+        }
+        bool escribio=fwrite(&reg, sizeof reg, 1, p);
+        fclose(p);
+        return escribio;
+    }
+    /*
+    int buscarRegistro(int a) {
+        Genero reg;
+        int cont=0;
+        FILE *p=fopen(_nombre,"ab+"); //para que funcione si es el prim.
+        if (p==NULL) {
+            cout<<"ERROR DE ARCHIVO"<<endl;
+
+            system("pause");
+            return -2;
+        }
+        while(fread(&reg, sizeof reg, 1, p)==1) {
+            if(reg.getDNI()==a&&reg.getEstado()!=false) {
+                fclose(p);
+                return cont;
+            }
+            cont++;
+        }
+        fclose(p);
+        return -1;
+    }
+    */ //BORRAR
+
+    bool modificarRegistro(int pos, Pelicula  reg) {
+        FILE *p=fopen(_nombre,"rb+");
+        if (p==NULL) {
+            cout<<"ERROR DE ARCHIVO"<<endl;
+            system("pause");
+            return false;
+        }
+        fseek(p,sizeof(Pelicula )*pos, 0);
+        bool esc=fwrite(&reg, sizeof reg, 1, p);
+        fclose(p);
+        return esc;
+    }
+
+    Pelicula leerRegistro(int pos) {
+        Pelicula reg;
+        reg.setEstado(false);
+        FILE *p=fopen(_nombre,"rb");
+        if (p==NULL) {
+            cout<<"ERROR DE ARCHIVO"<<endl;
+            system("pause");
+            return reg;
+        }
+        fseek(p,sizeof(Pelicula)*pos, 0);
+        fread(&reg, sizeof reg, 1, p);
+        fclose(p);
+        return reg;
+    }
+
+    int contarRegistros() {
+        FILE *p=fopen(_nombre,"ab+");
+        if (p==NULL) {
+            cout<<"ERROR DE ARCHIVO"<<endl;
+            return -2;
+        }
+        fseek(p,0,2);
+        int cantByte=ftell(p);//devuelve la cant de bytes de principio al puntero, osea el fin
+        fclose(p);
+        return (cantByte / sizeof(Pelicula));
+    }
+};
 
 
 #endif // CLASE_ARCHIVOPELICULAS_H_INCLUDED
--- clase_ArchivoUsuarios.h
+++ clase_ArchivoUsuarios.h
@@ -2,5 +2,109 @@
 #define CLASE_ARCHIVOUSUARIOS_H_INCLUDED
 
 
+class ArchivoUsuarios {
+  private:
+    char _nombre[30];
+  public:
+    ArchivoUsuarios(const char *nom) {
+        strcpy(_nombre,nom);
+    }
+    const char *getNombre() {
+        return _nombre;
+    }
+    void setNombre(const char *nom) {
+        strcpy(_nombre,nom);
+    }
+
+    bool listarRegistros() {
+        Usuario reg;
+        FILE *p=fopen(_nombre,"rb");
+        if (p==NULL) {
+            cout<<"ERROR DE ARCHIVO"<<endl;
+            system("pause");
+            return false;
+        }
+        fseek(p, 0, 1);
+        while(fread(&reg, sizeof reg, 1, p)==1) {
+            if (reg.getEstado()==true)reg.Mostrar();
+        }
+        fclose(p);
+        return true;
+    }
+
+    bool agregarRegistro(Usuario reg) {
+        FILE *p=fopen(_nombre,"ab");
+        if (p==NULL) {
+            cout<<"ERROR DE ARCHIVO"<<endl;
+            system("pause");
+            return false;
+        }
+        bool escribio=fwrite(&reg, sizeof reg, 1, p);
+        fclose(p);
+        return escribio;
+    }
+    /*
+    int buscarRegistro(int a) {
+        Genero reg;
+        int cont=0;
+        FILE *p=fopen(_nombre,"ab+"); //para que funcione si es el prim.
+        if (p==NULL) {
+            cout<<"ERROR DE ARCHIVO"<<endl;
+
+            system("pause");
+            return -2;
+        }
+        while(fread(&reg, sizeof reg, 1, p)==1) {
+            if(reg.getDNI()==a&&reg.getEstado()!=false) {
+                fclose(p);
+                return cont;
+            }
+            cont++;
+        }
+        fclose(p);
+        return -1;
+    }
+    */ //BORRAR
+
+    bool modificarRegistro(int pos, Usuario reg) {
+        FILE *p=fopen(_nombre,"rb+");
+        if (p==NULL) {
+            cout<<"ERROR DE ARCHIVO"<<endl;
+            system("pause");
+            return false;
+        }
+        fseek(p,sizeof(Usuario)*pos, 0);
+        bool esc=fwrite(&reg, sizeof reg, 1, p);
+        fclose(p);
+        return esc;
+    }
+
+    Usuario leerRegistro(int pos) {
+        Usuario reg;
+        reg.setEstado(false);
+        FILE *p=fopen(_nombre,"rb");
+        if (p==NULL) {
+            cout<<"ERROR DE ARCHIVO"<<endl;
+            system("pause");
+            return reg;
+        }
+        fseek(p,sizeof(Usuario)*pos, 0);
+        fread(&reg, sizeof reg, 1, p);
+        fclose(p);
+        return reg;
+    }
+
+    int contarRegistros() {
+        FILE *p=fopen(_nombre,"ab+");
+        if (p==NULL) {
+            cout<<"ERROR DE ARCHIVO"<<endl;
+            return -2;
+        }
+        fseek(p,0,2);
+        int cantByte=ftell(p);//devuelve la cant de bytes de principio al puntero, osea el fin
+        fclose(p);
+        return (cantByte / sizeof(Usuario));
+    }
+};
 
 #endif // CLASE_ARCHIVOUSUARIOS_H_INCLUDED
